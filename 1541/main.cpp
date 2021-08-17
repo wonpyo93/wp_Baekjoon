df9c11f549fe6e0f37f1f3e744eaefa99ebc2643
wonpyo93 <wonpyo93@gmail.com>
2021-08-17 22:54:09 +0900

    1541 solved

--- 1541/main.cpp
+++ 1541/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
+using namespace std;
+
+string input, temp = "";
+bool check;
+int ans = 0;
 
 int main() {
+    cin >> input;
+    for(int i = 0; i <= input.size(); i++) {
+        if (input[i] == '+' || input[i] == '-' || input[i] == '\0') {
+            if(check)
+                ans -= stoi(temp);
+            else
+                ans += stoi(temp);
+            temp = "";
+            if (input[i] == '-')
+                check = true;
+        }
+        else
+            temp += input[i];
+    }
+
+    cout << ans;
     return 0;
 }
