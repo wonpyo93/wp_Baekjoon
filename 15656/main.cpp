18d5037dbd6c83b0f6f0bc9d603313802aac538b
wonpyo93 <wonpyo93@gmail.com>
2021-08-19 18:16:59 +0900

    15656 solved

---
+++ 15656/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 8
+using namespace std;
+
+int N, M, map[MAX], visited[MAX];
+
+void dfs(int index) {
+	if (index == M) {
+		for (int i = 0; i < M; i++)
+			cout << map[visited[i]] << " ";     
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		visited[index] = i;
+        dfs(index + 1);
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		cin >> map[i];
+	sort(map, map + N);
+	dfs(0);
+
+	return 0;
+}
--- 15666/main.cpp
+++ 15666/main.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <map>
 #define MAX 8
 using namespace std;
 
-int N, M, map[MAX], visited[MAX];
+int N, M, arr[MAX], input;
+map<int, bool> visited;
+vector<int> v;
 
-void dfs(int index) {
-	if (index == M) {
+void dfs(int index, int start, int count) {
+	if (count == M) {
 		for (int i = 0; i < M; i++)
-			cout << map[visited[i]] << " ";     
+			cout << v[arr[i]] << " ";
 		cout << "\n";
 		return;
 	}
 
-	for (int i = 0; i < N; i++) {
-		visited[index] = i;
-		dfs(index + 1);
+	for (int i = start; i < v.size(); i++) {
+		arr[index] = i;
+		dfs(index + 1, i, count + 1);
 	}
 }
 
 int main() {
 	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-		cin >> map[i];
-	sort(map, map + N);
-	dfs(0);
-
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		if (!visited.count(input)) {
+			visited[input] = true;
+			v.push_back(input);
+		}
+	sort(v.begin(), v.end());
+	dfs(0, 0, 0);
 	return 0;
 }
