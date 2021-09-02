5672bef7760c57d2826d6cb165a8db0bbb294988
wonpyo93 <wonpyo93@gmail.com>
2021-09-02 16:28:29 +0900

    Update main.cpp

--- 1112/main.cpp
+++ 1112/main.cpp
@@ -2,29 +2,41 @@
 #include <algorithm>
 using namespace std;
 
-long long X, x, B;
+int N, b, m;
+
+int mod(int N, int b) {
+    int r = N % abs(b);
+    if(r < 0) r += abs(b);
+    return r;
+}
+
+int base(int N, int b, char r[]) {
+    int na = b > 0 ? abs(N) : N, t;
+    int c = 0;
+    while(na) {
+        t = mod(na, b);
+        na = (na - t) / b;
+        r[c++] = t + '0';
+    }
+    if(!c)
+        r[c++] = '0';
+    r[c] = 0;
+    return (b > 0 && N < 0);
+}
+
+void print(char r[], int m) {
+    int i;
+    if(m)
+        putchar('-');
+    for(i = 1; r[i] ; i++);
+    for(i-- ; i >= 0; i--)
+        putchar(r[i]);
+    putchar('\n');
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> X >> B;
-    x = X;
-
-    //preprocessing
-    if(B > 0) { //양수의 진수일 때
-        if(X < 0) { //X가 음수일 때
-            X = -X;
-        }
-    }
-    else { //음수의 진수일 때
-        
-    }
-
-    while(x != 0) {
-
-    }
-
-    return 0;
+    char r[64];
+    cin >> N >> b;
+    m = base(N, b, r);
+    print(r, m);
 }
