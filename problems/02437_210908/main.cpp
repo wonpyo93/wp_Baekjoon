dcc8c711b4ec3787f8cbbd67df95d446afb31858
wonpyo93 <wonpyo93@gmail.com>
2021-09-08 17:19:59 +0900

    2437 solved

--- problems/02437_210908/main.cpp
+++ problems/02437_210908/main.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+#define ll long long
 using namespace std;
 
-int N, temp;
-priority_queue<int, vector<int>, greater<int>> Q;
+int N;
+ll tmp, sum;
+vector<ll> vec;
 
 int main() {
-    ios::sync_with_stdio(0);
-	cin.tie(0);
     cin >> N;
     for(int i = 0; i < N; i++) {
-        cin >> temp;
-        if(temp == 0) {
-            if(Q.size() > 0) {
-                cout << Q.top() << '\n';
-                Q.pop();
-            }
-            else
-                cout << 0 << '\n';
-        }
-        else Q.push(temp);
+        cin >> tmp;
+        vec.push_back(tmp);
     }
-	return 0;
+    
+    sort(vec.begin(), vec.end());
+    
+    //print sorted
+    // for(int i = 0; i < vec.size(); i++)
+    //     cout << vec[i] << " ";
+    // cout << endl;
+
+    sum = 1;
+    if(vec[0] != 1)
+        cout << 1;
+    else {
+        for(int i = 1; i < N; i++) {
+            if(vec[i] > sum + 1) {
+                // cout << vec[i] << " " << sum + 1 << endl;
+                break;
+            }
+            sum += vec[i];
+            // cout << "sum = " << sum << endl;
+        }
+        cout << sum + 1;
+    }
+    return 0;
 }
